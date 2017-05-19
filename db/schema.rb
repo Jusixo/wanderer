7914ded9ac5ee0d5a90bc317aa80586347fa6cf2
Justin Valentin <Justinvalentine88@gmail.com>
2017-05-19 18:00:10 -0400

    database created, events table created

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 20170519214450) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
