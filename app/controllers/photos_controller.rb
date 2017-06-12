4543f6ae31d6a845a2063b6c0d7463f4958238b0
Justin Valentin <Justinvalentine88@gmail.com>
2017-06-12 16:05:27 -0400

    updated url pull from livestream so it matches with youtube live.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,7 +16,7 @@ class PhotosController < ApplicationController
 
     %x{
       ffmpeg -y -headers "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7^M\012Accept-Language: en-us,en;q=0.5^M\012Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8^M\012User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/47.0 (Chrome)^M\012" \
-        -i $(youtube-dl https://youtu.be/liEbIbGJRO8 --get-url) \
+        -i $(youtube-dl https://youtu.be/rmIAhATfLMo --get-url) \
         -ss 00:00:01.500 -f image2 -vframes 1 #{thumbnail_temp_path}
     }
 
--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -10,7 +10,7 @@ class ImageUploader < Shrine
   process(:store) do |io, context|
     original = io.download
 
-    size_800 = resize_to_fit(original, 800, 800)
+    size_800 = resize_to_fill(original, 800, 800) { |cmd| cmd.auto_orient } # orient rotated images
     size_500 = resize_to_fill(size_800, 500, 500)
     size_300 = resize_to_fit(size_500, 500, 500)
     size_thumbnail = resize_to_fill(size_300, 64, 64)
