9c849bb7ad4713d94891e2bb8940ca14de1bbe29
Justin Valentin <Justinvalentine88@gmail.com>
2017-06-13 17:56:30 -0400

    resized event photos, turned get photos button into a static modal.

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -31,4 +31,15 @@ $(document).ready(function() {
       }
     })
   })
+
+  $('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+  })
+
+  $('#modal-container').click(function(){
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+  });
 })
