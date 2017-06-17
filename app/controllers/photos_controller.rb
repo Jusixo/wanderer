ad2286849e618606051b19cf04948779c57904fd
Justin Valentin <Justinvalentine88@gmail.com>
2017-06-17 13:45:22 -0400

    updating youtube url to push to heroku

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,11 +16,11 @@ class PhotosController < ApplicationController
 
     %x{
       ffmpeg -y -headers "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7^M\012Accept-Language: en-us,en;q=0.5^M\012Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8^M\012User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/47.0 (Chrome)^M\012" \
-        -i $(youtube-dl https://youtu.be/rmIAhATfLMo --get-url) \
+        -i $(youtube-dl https://youtu.be/https://AuBEBvfRUKE --get-url) \
         -ss 00:00:01.500 -f image2 -vframes 1 #{thumbnail_temp_path}
     }
 
-    @event  = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id])
     @photo = @event.photos.new(image: File.open(thumbnail_temp_path))
 
     if @photo.save
@@ -32,7 +32,7 @@ class PhotosController < ApplicationController
 
   # POST /photos
   def create
-    @event  = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id])
     @photo = @event.photos.new(photo_params)
 
     if @photo.save
